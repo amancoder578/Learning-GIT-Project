8217e6237475c4e5543ef05067dc3259d8b91a2e
aman <amaaanahmedkhan987@gmail.com>
2023-05-21 13:37:02 +0530

    adding script.js file

--- gitFolder/script.js
+++ gitFolder/script.js
@@ -1,6 +1,5 @@
-<script>
+ <script>
       // JavaScript code can be directly included within the <script> tags.
       function myFunction() {
         alert("Hello, world!");
       }
-    </script>
